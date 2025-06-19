d0d4c010c1bf684fd25490ab43e201ca4d80f046
otaviosbms <otaviosbms@hotmail.com>
2025-06-18 21:55:36 -0300

    adding view engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public')); // opcional
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  // Usando express diretamente para body parser
+  app.use(express.urlencoded({ extended: true }));
+
+  await app.listen(3000);
 }
 bootstrap();
